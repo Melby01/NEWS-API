e056c21fa50e9244d78ebe7ef8d8d50d5ab48781
kelvinhovinho <melby.okozi@student.moringaschool.com>
2021-06-11 03:13:26 +0300

    Update the project

--- app/main/errors.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from . import main
-@main.app_errorhandler(FourOwFour)
-def (error):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('404.html'),FourOwFour
--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
-
-class ReviewForm(FlaskForm):
-
-    title = StringField('Articles title',validators=[Required()])
-    review = TextAreaField('News review', validators=[Required()])
-    submit = SubmitField('Submit')
